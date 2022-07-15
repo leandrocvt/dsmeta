58c70e309c7913af52868125361586a61a3750df
Leandro Cavalcanti <leandrocavalcanti499@gmail.com>
2022-07-15 14:06:31 -0300

    useState

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,6 +1,6 @@
-import logo from '../../assets/img/logo.svg'
+import logo from '../../assets/img/logo.svg';
 
-import './styles.css'
+import './styles.css';
 
 function Header() {
     return (
@@ -10,11 +10,11 @@ function Header() {
                 <h1>DSMeta</h1>
                 <p>
                     Desenvolvido por
-                    <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+                    <a href="https://github.com/leandrocvt/dsmeta">@leandrocvt</a>
                 </p>
             </div>
         </header>
     )    
   }
 
-export default Header
+export default Header;
